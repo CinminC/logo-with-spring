c1504666315f35cd3d25fb7b0129decd4497c117
Cinmin Chen <cinminchen212@gmail.com>
2024-06-25 15:07:36 +0800

    add sceond ImageManager
    rewrite applyForce()

--- sketch.js
+++ sketch.js
@@ -60,6 +60,10 @@ class ImageManager {
         return totalWidth;
     }
 
+    updatePosY(spacing) {
+        this.startY += this.imageElements[0].height * this.imageScale + spacing;
+    }
+
     drawDebug() {
         for (let constraint of this.constraints) {
             push();
@@ -75,6 +79,7 @@ class ImageManager {
             let y = this.startY;
             push();
             fill(20);
+            noStroke()
             rect(x + imageWidth / 2, y, img.width * this.imageScale, img.height * this.imageScale);
             pop();
             x += imageWidth + this.imageSpacing;
@@ -112,13 +117,27 @@ class ImageManager {
             World.add(world, constraint);
         }
     }
-
     applyForces() {
         let forceMagnitude = map(mouseX, 0, width, 0, force);
-        Body.applyForce(this.rect[0], { x: this.rect[0].position.x, y: this.rect[0].position.y }, { x: -forceMagnitude, y: 0 });
-        Body.applyForce(this.rect[1], { x: this.rect[1].position.x, y: this.rect[1].position.y }, { x: -forceMagnitude, y: 0 });
-        Body.applyForce(this.rect[2], { x: this.rect[2].position.x, y: this.rect[2].position.y }, { x: forceMagnitude, y: 0 });
-        Body.applyForce(this.rect[3], { x: this.rect[3].position.x, y: this.rect[3].position.y }, { x: forceMagnitude, y: 0 });
+        let midIndex = Math.floor(this.rect.length / 2);
+
+        for (let i = 0; i < this.rect.length; i++) {
+            let direction = 0;
+
+            if (i < midIndex) {
+                direction = -forceMagnitude;
+            } else if (i > midIndex || this.rect.length % 2 === 0) {
+                direction = forceMagnitude;
+            }
+
+            Body.applyForce(this.rect[i], { x: this.rect[i].position.x, y: this.rect[i].position.y }, { x: direction, y: 0 });
+        }
+
+        // Body.applyForce(this.rect[0], { x: this.rect[0].position.x, y: this.rect[0].position.y }, { x: -forceMagnitude, y: 0 });
+        // Body.applyForce(this.rect[1], { x: this.rect[1].position.x, y: this.rect[1].position.y }, { x: -forceMagnitude, y: 0 });
+        // Body.applyForce(this.rect[2], { x: this.rect[2].position.x, y: this.rect[2].position.y }, { x: forceMagnitude, y: 0 });
+        // Body.applyForce(this.rect[3], { x: this.rect[3].position.x, y: this.rect[3].position.y }, { x: forceMagnitude, y: 0 });
+
     }
 
     updateDamping() {
@@ -176,16 +195,11 @@ let force = 0.01
 let whValue = 400;
 let imageManager1;
 function preload() {
-    // for (let i = 0; i < imagePaths.length; i++) {
-    //     images[i] = loadImage(imagePaths[i]);
-    // }
-    // for (let i = 0; i < imagePaths2.length; i++) {
-    //     images2[i] = loadImage(imagePaths2[i]);
-    // }
-
-    imageManager1 = new ImageManager(['img/college.png', 'img/of.png', 'img/humanities.png', 'img/arts.png'], whValue / 2);
+    imageManager1 = new ImageManager(imagePaths, whValue / 2);
+    imageManager2 = new ImageManager(imagePaths2, whValue / 2);
 
     imageManager1.preloadImages();
+    imageManager2.preloadImages();
 }
 function setup() {
     createCanvas(whValue, whValue);
@@ -200,35 +214,9 @@ function setup() {
 
 
     imageManager1.initialize();
-    // //init p
-    // let totalWidth = calculateTotalWidth(images)
-    // startX = (width - totalWidth) / 2
-    // let x = startX
-    // for (let i = 0; i < imagePaths.length; i++) {
-    //     let imageWidth = images[i].width * imageScale;
-    //     let y = height / 2;
-    //     let circle = Bodies.rectangle(x + imageWidth / 2, y, images[i].width * imageScale, images[i].height * imageScale, { restitution: 1, friction: 0.5 });
-    //     texts.push(circle);
-    //     initialPositions.push({ x: circle.position.x, y: circle.position.y }); // save init position
-    //     World.add(world, circle);
-    //     print(totalWidth)
-    //     print(x + imageWidth / 2)
-    //     x += imageWidth + imageSpacing;
-    // }
 
-    // //set up constraints
-    // for (let i = 0; i < texts.length - 1; i++) {
-    //     let options = {
-    //         bodyA: texts[i],
-    //         bodyB: texts[i + 1],
-    //         length: texts[i + 1].position.x - texts[i].position.x,
-    //         stiffness: 0.1
-    //     };
-    //     let constraint = Constraint.create(options);
-    //     constraints.push(constraint);
-    //     World.add(world, constraint);
-    // }
-
+    imageManager2.updatePosY(8);
+    imageManager2.initialize();
 
     // mouse drage(maybe dont need)
     let canvasMouse = Mouse.create(canvas.elt);
@@ -246,128 +234,27 @@ function draw() {
     Engine.update(engine);
     rectMode(CENTER)
 
-    // if (showDebug) {
-    //     // draw constraints
-    //     for (let constraint of constraints) {
-    //         push()
-    //         stroke(0);
-    //         strokeWeight(2);
-    //         line(constraint.bodyA.position.x, constraint.bodyA.position.y, constraint.bodyB.position.x, constraint.bodyB.position.y);
-    //         pop()
-    //     }
-
-    //     let x = startX
-    //     for (let i = 0; i < 4; i++) {
-    //         let imageWidth = images[i].width * imageScale;
-    //         let y = height / 2;
-    //         push()
-    //         fill(20)
-    //         rect(x + imageWidth / 2, y, images[i].width * imageScale, images[i].height * imageScale);
-    //         pop()
-    //         x += imageWidth / 2 + imageSpacing + imageWidth / 2;
-    //     }
-    //     // 繪製鼠標拖動的約束線
-    //     // if (mConstraint.body) {
-    //     //     let pos = mConstraint.body.position;
-    //     //     let offset = mConstraint.constraint.pointB;
-    //     //     let m = mConstraint.mouse.position;
-    //     //     stroke(0, 255, 0);
-    //     //     line(pos.x + offset.x, pos.y + offset.y, m.x, m.y);
-    //     // }
-    // }
-
-
-    // // calculate new pos, set new constraints
-    // if (mouseIsPressed) {
-    //     imageSpacing = map(mouseX, 0, width, 10, 80)
-
-    //     let totalWidth = calculateTotalWidth(images)
-    //     startX = (width - totalWidth) / 2
-    //     let xx = startX
-    //     for (let i = 0; i < texts.length; i++) {
-    //         let imageWidth = images[i].width * imageScale;
-
-    //         if (i < texts.length - 1) {
-    //             Composite.remove(world, constraints[i]);
-    //         }
-
-    //         initialPositions[i].x = xx + imageWidth / 2
-    //         xx += imageWidth + imageSpacing;
-
-    //     }
-    //     constraints = []
-
-    //     for (let i = 0; i < texts.length - 1; i++) {
-    //         let options = {
-    //             bodyA: texts[i],
-    //             bodyB: texts[i + 1],
-    //             length: initialPositions[i + 1].x - initialPositions[i].x,
-    //             stiffness: 0.1
-    //         };
-    //         let constraint = Constraint.create(options);
-    //         constraints.push(constraint);
-    //         World.add(world, constraint);
-    //     }
-
-    //     //apply force
-    //     let forceMagnitude = map(mouseX, 0, width, 0, force);
-    //     Body.applyForce(texts[0], { x: texts[0].position.x, y: texts[0].position.y }, { x: -forceMagnitude, y: 0 });
-    //     Body.applyForce(texts[1], { x: texts[1].position.x, y: texts[1].position.y }, { x: -forceMagnitude, y: 0 });
-    //     Body.applyForce(texts[2], { x: texts[2].position.x, y: texts[2].position.y }, { x: forceMagnitude, y: 0 });
-    //     Body.applyForce(texts[3], { x: texts[3].position.x, y: texts[3].position.y }, { x: forceMagnitude, y: 0 });
-
-    // }
-
-    // // 設置阻尼使物體逐漸減速回到原位
-    // for (let circle of texts) {
-    //     Body.setVelocity(circle, { x: circle.velocity.x * 0.85, y: circle.velocity.y }); // 這裡的 0.9 是阻尼因子，控制減速的快慢
-    // }
-
-    // // 逐漸回到初始位置的運動
-    // for (let i = 0; i < texts.length; i++) {
-    //     let circle = texts[i];
-    //     let initialPos = initialPositions[i];
-    //     let currPos = circle.position;
-    //     let distX = initialPos.x - currPos.x;
-    //     let distY = initialPos.y - currPos.y;
-    //     let easing = 0.05; // 調整回到原位的速度
-    //     if (Math.abs(circle.velocity.x) < 0.1 && Math.abs(circle.velocity.y) < 0.1) {
-    //         circle.position.x += distX * easing;
-    //         circle.position.y += distY * easing;
-    //     }
-    // }
-
-
-    // let count = 0;
-    // for (let circle of texts) {
-    //     fill(255);
-    //     // stroke(0);
-    //     // strokeWeight(1);
-    //     rectMode(CENTER)
-    //     imageMode(CENTER)
-    //     let imgTemp = images[count]
-    //     image(imgTemp, circle.position.x, circle.position.y, imgTemp.width * imageScale, imgTemp.height * imageScale);
-    //     // rect(circle.position.x, circle.position.y, circle.bounds.max.x - circle.bounds.min.x, circle.bounds.max.y - circle.bounds.min.y);
-    //     count++
-    // }
-
-
-
     if (showDebug) {
         imageManager1.drawDebug();
+        imageManager2.drawDebug();
     }
 
     if (mouseIsPressed) {
         imageManager1.updatePositions();
+        imageManager2.updatePositions();
 
         imageManager1.applyForces();
+        imageManager2.applyForces();
     }
 
     imageManager1.updateDamping();
+    imageManager2.updateDamping();
 
     imageManager1.updateEasing();
+    imageManager2.updateEasing();
 
     imageManager1.drawImages();
+    imageManager2.drawImages();
 }
 
 
@@ -377,16 +264,5 @@ function keyPressed() {
         print('keyPressed')
     }
 
-}
-
-function calculateTotalWidth(images) {
-    let totalWidth = 0;
-    for (let i = 0; i < images.length; i++) {
-        totalWidth += images[i].width * imageScale;
-        if (i < images.length - 1) {
-            totalWidth += imageSpacing;
-        }
-    }
-    return totalWidth;
 }
 
